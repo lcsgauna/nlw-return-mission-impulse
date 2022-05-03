5d15e9a8ac98dc72baee5f1db2d823b23d594908
Lucas Gauna <lucasgsamudio@gmail.com>
2022-05-02 22:43:28 -0400

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+interface ButtonProps{
+  text?:string;
+}
+
+function Button(props:ButtonProps){
+  return <button className="bg-violet-500 p-2">{props.text ?? "Default"}</button>
+}
+
+function App() {
+  return (
+    <div>
+      <Button text="Enviar"/>
+      <Button text="Ok"/>
+      <Button/>
+    </div>
+  )
+}
+
+export default App
